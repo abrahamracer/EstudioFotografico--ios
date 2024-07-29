eba5aeb7790ea4e8a9cf3cb1115bcad015878c72
Abraham Valderrabano Vega <abraham171270@gmail.com>
2024-07-28 21:28:12 -0600

    Initial Commit

---
+++ EstudioFotografico/EstudioFotograficoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EstudioFotograficoApp.swift
+//  EstudioFotografico
+//
+//  Created by Abraham Valderrabano Vega on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct EstudioFotograficoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
